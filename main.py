9f2235ababb86d680aac104b08bd6dfa538b7ffa
Domi456 <121254412+Domi456@users.noreply.github.com>
2025-05-08 14:57:51 +0200

    some mods

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
 from PIL import Image
 import os
+import random
 
 load_dotenv()
 hf_token = os.getenv("HF_HUB_API_KEY")
+
+prompt = input("Enter a prompt for the image generation: ")
+random_num = random.randrange(1, 1000)
 
 client = InferenceClient(
     provider="replicate",
@@ -14,8 +18,9 @@ client = InferenceClient(
 print("Starting inference...")
 
 image = client.text_to_image(
-    "An astronaut riding a horse on Mars", 
+    prompt=prompt, 
     model="black-forest-labs/FLUX.1-dev"
 )
 
-image.save("output.png")
+image.save(f"output_{random_num}.png")
+print(f"Inference completed. Image saved as output_{random_num}.png")
